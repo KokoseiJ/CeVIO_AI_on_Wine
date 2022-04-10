198ce8a699acaab33114fe58de5698935a2edfe1
KokoseiJ <kokoseij@gmail.com>
2022-04-09 20:29:15 -0700

    Removes useless pragma links

---
+++ exportgen.py
@@ -0,0 +1,20 @@
+import re
+import pefile
+
+dllpath = input("DLL dir: ")
+dll = pefile.PE(dllpath)
+
+repath = re.sub(r"\.[dD][lL]{2}", "", dllpath).replace("\\", "\\\\")
+
+pragma = [
+	f"""#pragma comment(linker, "/export:{symbol.name.decode()}=\\"{repath}.{symbol.name.decode()}\\"")"""
+	for symbol in dll.DIRECTORY_ENTRY_EXPORT.symbols
+	if symbol.name
+]
+
+pragmastr = "\n".join(pragma)
+
+print(pragmastr)
+
+with open("pragma.cpp", "w") as f:
+	f.write(pragmastr)
