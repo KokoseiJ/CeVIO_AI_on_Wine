a292d3b4e161b688e30c78030ab3537596f99227
KokoseiJ <kokoseij@gmail.com>
2022-11-08 22:18:49 -0800

    Added .def output to exportgen.py

--- exportgen.py
+++ exportgen.py
@@ -4,18 +4,31 @@ import pefile
 dllpath = input("DLL dir: ")
 dll = pefile.PE(dllpath)
 
-repath = re.sub(r"\.[dD][lL]{2}", "", dllpath).replace("\\", "\\\\")
+repath_defs = re.sub(r"\.[dD][lL]{2}", "", dllpath)
+repath = repath_defs.replace("\\", "\\\\")
 
 pragma = [
-	f"""#pragma comment(linker, "/export:{symbol.name.decode()}=\\"{repath}.{symbol.name.decode()}\\"")"""
-	for symbol in dll.DIRECTORY_ENTRY_EXPORT.symbols
-	if symbol.name
+    f"""#pragma comment(linker, "/export:{symbol.name.decode()}=\\"{repath}.{symbol.name.decode()}\\"")"""
+    for symbol in dll.DIRECTORY_ENTRY_EXPORT.symbols
+    if symbol.name
+]
+
+defs = [
+    f"""  {symbol.name.decode()}="{repath_defs}".{symbol.name.decode()}"""
+    for symbol in dll.DIRECTORY_ENTRY_EXPORT.symbols
+    if symbol.name
 ]
 
 pragmastr = "\n".join(pragma)
+defsstr = "EXPORTS\n" + "\n".join(defs)
 
 print(pragmastr)
+print()
+print(defsstr)
 
 with open("pragma.cpp", "w") as f:
-	f.write(pragmastr)
+    f.write(pragmastr)
+
+with open("export.def", "w") as f:
+    f.write(defsstr)
 
